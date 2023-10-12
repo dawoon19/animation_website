538dbdeb865f83e21246d4bd0561d46017ec4a8e
Daniel Won <danielwon23@berkeley.edu>
2023-10-11 17:36:19 -0700

    basic backend & frontend for canvas creation

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,35 @@
 const express = require('express')
+const fs = require('fs')
 const app = express()
 const port = 8081
+
+const projects = require('./projects.json');
+
+var cors = require('cors');
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.put('/projects/:id', (req, res) => {
-    req.params.id
+app.put('/projects/init-project/:width/:height', (req, res) => {
+    let width = Number(req.params.width);
+    let height = Number(req.params.height);
+    let projectId = projects.length;
+
+    console.log(req.params);
+
+    let json = {
+        id:projectId,
+        width:width,
+        height:height
+    }
+    
+    projects.push(json);
+    fs.writeFile('./projects.json', JSON.stringify(projects), err => {
+        if (err) console.log(err);
+    })
+    res.send({id: projectId});
 })
 
 app.listen(port, () => {
--- frontend/src/components/Startup.js
+++ frontend/src/components/Startup.js
@@ -1,14 +1,27 @@
 import '../styles/UI.css'
 import {useState} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import config from '../config.json';
+import axios from 'axios';
 
 
 export default function Startup(props) {
     const [width, setWidth] = useState(undefined);
     const [height, setHeight] = useState(undefined);
+    const nav = useNavigate();
 
     function submit() {
-        fetch("https://localhost:8081/")
+        if (width === undefined || height === undefined || width <= 100 || height <= 100) {
+            alert('ERROR: Please put in valid canvas size.')
+        } else {
+            axios.put(config.API_URL + "/projects/init-project/" + width + "/" + height).then(res => {
+                alert('Successfully created project.');
+                nav('/projects/' + res.data.id)
+            }).catch(err =>  {
+                alert('Unable to connect to server.')
+                console.log(err);
+            })
+        }
     }
 
     return <div className='main'>
@@ -19,7 +32,7 @@ export default function Startup(props) {
                 <input type="number" placeholder="Enter width..." onChange={(e)=> setWidth(e.target.value)}></input> x
                 <input type="number" placeholder="Enter height..." onChange={(e)=> setHeight(e.target.value)}></input>
             </form>
-            <Link to="/projects/1" class="button" onClick={submit}>Start Animating</Link>
+            <div class="button" onClick={submit}>Start Animating</div>
         </div>
 
     </div>
